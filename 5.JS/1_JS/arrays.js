bcaa035e317f5fa6b90fbaeb39e97541f35c3c14
Jefferson Espitia <Jefferespitia@gmail.com>
2026-02-13 10:25:23 +0100

    Add-firts-part-class-today

---
+++ 5.JS/1_JS/arrays.js
@@ -0,0 +1,10 @@
+const user = ['Pepe', 22, 'Developer']
+console.log(user)
+console.log(user[0])
+
+// Excentricidad
+user.id = 1
+console.log(user)
+
+
+const usersContacts = ['Luis', 'Ramon', 'Rosa']
